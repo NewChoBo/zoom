7bbb149da9197153153d764102e076a0bd1d22bc
NewChoBo <whwjdrb1009@gmail.com>
2023-02-13 18:28:27 +0900

    #1.4 WebSocket Messages (12:37) 웹소켓 이벤트들 실습

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -3,7 +3,25 @@
 
 //window.location, window.location.host 명령어를 통해 사용자의 현재 주소를 알 수 있다.
 //app.js의 socket은 서버로의 연결을 의미한다.
+
+//서버와 연결
 const socket = new WebSocket(`ws://${window.location.host}`);  
 
+//서버와 연결되는 이벤트
+socket.addEventListener("open", () => {
+    console.log("Connected to Server ✔");
+});
 
+//서버에게 메세지를 받는 이벤트
+socket.addEventListener("message", (message) => {
+    console.log("New message: ", message.data);
+});
 
+//서버와 연결이 끊길때 이벤트
+socket.addEventListener("close", () => {
+    console.log("Disconnected from Server 😡");
+});
+
+setTimeout(() => {
+    socket.send("hello from the browser!");
+}, 10000);
--- src/server.js
+++ src/server.js
@@ -10,7 +10,8 @@ app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.render("/"));
+// app.get("/*", (req, res) => res.render("/"));
+app.get("/*", (req, res) => res.render("home"));
 
 const handleListen = () => console.log('Listening on http://localhost:3000');
 // app.listen(3000, handleListen);
@@ -19,12 +20,24 @@ const server = http.createServer(app);
 // const wss = new WebSocketServer.Server({ server });
 const wss = new WebSocket.Server({ server });
 
-//server.js의 socket은 연결된 브라우저를 뜻한다.
-function handleConnection(socket){
-    console.log(socket);
-}
 
 //web socket도 event가 있다.
-wss.on("connection", handleConnection);
+wss.on("connection", (socket) => {
+    // console.log(socket);
 
-server.listen(3000, handleListen);
+    //클라이언트랑 연결이 끊기면 동작
+    socket.on("close", () => {console.log("Disconnected from the Browser 😡");});
+
+    //클라이언트에게서 메세지 받음
+    socket.on("message", (message) => {
+        console.log(message.toString('utf-8'));
+    });
+
+    //클라이언트에게 메세지 전송
+    socket.send("hello!!!");
+
+
+    console.log("Connected to Browser ✔");
+});
+
+server.listen(3000, handleListen);      //포트번호, 함수 실행
