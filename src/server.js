8888d5c0f518f5d509556cdf70f8008b583f4a00
NewChoBo <whwjdrb1009@gmail.com>
2023-02-13 13:42:28 +0900

    #1.2 WebSockets in NodeJS (11:34) 웹소켓 서버 설정

--- src/server.js
+++ src/server.js
@@ -1,4 +1,7 @@
+import http from "http";
 import express, { application } from "express";
+// import WebSocketServer from "ws";
+import WebSocket from "ws";
 
 const app = express();
 
@@ -9,5 +12,11 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.render("/"));
 
-const handleListen = () => console.log('Listening on http://localhost:3000')
-app.listen(3000, handleListen);
+const handleListen = () => console.log('Listening on http://localhost:3000');
+// app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+// const wss = new WebSocketServer.Server({ server });
+const wss = new WebSocket.Server({ server });
+
+server.listen(3000, handleListen);
