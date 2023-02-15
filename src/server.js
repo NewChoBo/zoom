db8b366e5382b89ac9b65eea5f74ccabaf2f5591
NewChoBo <whwjdrb1009@gmail.com>
2023-02-15 20:27:07 +0900

    #2.6 Room Notifications (13:20) 메세지 보내기

--- src/server.js
+++ src/server.js
@@ -38,6 +38,17 @@ wsServer.on("connection", (socket) => {
         done();
         socket.to(roomName).emit("welcome");
     });
+
+    socket.on("disconnecting", () => {
+        socket.rooms.forEach(
+            room => socket.to(room).emit("bye")
+        );
+    });
+
+    socket.on("new_message", (msg, roomName, done) => {
+        socket.to(roomName).emit("new_message", msg);
+        done();
+    });
 });
 
 
