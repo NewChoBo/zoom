818284f807357f94c5ebf6bc9adeb3cddc63378a
NewChoBo <whwjdrb1009@gmail.com>
2023-02-15 21:12:58 +0900

    #2.7 Nicknames (09:44) 코드챌린지 전

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -94,12 +94,18 @@ function addMessage(message){
 
 function handleMessageSubmit(event){
     event.preventDefault();
-    const input = room.querySelector("input");
+    const input = room.querySelector("#msg input");
     const value = input.value;
     socket.emit("new_message", input.value, roomName, () => {
         addMessage(`You: ${value}`);
     });        //이벤트 이름은 상관 없음.
     input.value = "";
+}
+
+function handleNicknameSubmit(event){
+    event.preventDefault();
+    const input = room.querySelector("#name input");    //room에서 id가 name인 오브젝트의 input
+    socket.emit("nickname", input.value);
 }
 
 function showRoom(){
@@ -109,8 +115,11 @@ function showRoom(){
     const h3 = document.querySelector("h3");
     h3.innerText = `Room ${roomName}`;
     
-    const form = room.querySelector("form");
-    form.addEventListener("submit", handleMessageSubmit);
+    const msgForm = room.querySelector("#msg");
+    msgForm.addEventListener("submit", handleMessageSubmit);
+
+    const nameForm = room.querySelector("#name");
+    nameForm.addEventListener("submit", handleNicknameSubmit);
 }
 
 function handleRoomSubmit (event) {
@@ -137,12 +146,12 @@ form.addEventListener("submit", handleRoomSubmit);
 
 
 
-socket.on("welcome", () => {
-    addMessage("someone joined!");
+socket.on("welcome", (user) => {
+    addMessage(`${user} joined!`);
 });
 
-socket.on("bye", () => {
-    addMessage("someone left ㅠㅠ");
+socket.on("bye", (left) => {
+    addMessage(`${left} left ㅠㅠ`);
 });
 
 
--- src/server.js
+++ src/server.js
@@ -28,6 +28,8 @@ const wsServer = SocketIO(httpServer);
 
 //connection 받을 준비 끝
 wsServer.on("connection", (socket) => {
+    socket["nickname"] = "Anon";
+
     socket.onAny((event) => {
         console.log(`Socket Event:${event}`);
         // console.log(`You are in room : ${JSON.stringify(socket.rooms)}`);    //이건 아닌가봄.
@@ -36,19 +38,21 @@ wsServer.on("connection", (socket) => {
     socket.on("enter_room", (roomName, done) => {
         socket.join(roomName);
         done();
-        socket.to(roomName).emit("welcome");
+        socket.to(roomName).emit("welcome", socket.nickname);
     });
 
     socket.on("disconnecting", () => {
         socket.rooms.forEach(
-            room => socket.to(room).emit("bye")
+            room => socket.to(room).emit("bye", socket.nickname)
         );
     });
 
     socket.on("new_message", (msg, roomName, done) => {
-        socket.to(roomName).emit("new_message", msg);
+        socket.to(roomName).emit("new_message", `${socket.nickname}: ${msg}`);
         done();
     });
+
+    socket.on("nickname", nickname => socket["nickname"] = nickname);
 });
 
 
