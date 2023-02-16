645ac4bfb0dd3892bc63f52a99ce3e1949b1166b
NewChoBo <whwjdrb1009@gmail.com>
2023-02-16 21:19:36 +0900

    #3.1 Call Controls (10:45) 장치 접근, 카메라 접근, 활성화/비활성화...

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,176 +1,73 @@
-/* web socket으로 작업한 파일
-
-// const aWebSocket = new WebSocket("http://localhost:3000/");      //웹소켓이기때문에 http 아님
-// const aWebSocket = new WebSocket("ws://localhost:3000/");
-
-//window.location, window.location.host 명령어를 통해 사용자의 현재 주소를 알 수 있다.
-//app.js의 socket은 서버로의 연결을 의미한다.
-//서버와 연결
-// const socket = new WebSocket(`ws://${window.location.host}`);  
-
-const messageList = document.querySelector("ul");
-// const messageForm = document.querySelector("form");
-const messageForm = document.querySelector("#message");     //id로 접근
-const nickForm = document.querySelector("#nick");
-
-//서버와 연결
-const socket = new WebSocket(`ws://${window.location.host}`);  
-
-function makeMessage(type, payload){
-    const msg = {type, payload}
-    return JSON.stringify(msg);
-}
-
-//서버와 연결되는 이벤트
-socket.addEventListener("open", () => {
-    console.log("Connected to Server ✔");
-});
-
-//서버에게 메세지를 받는 이벤트
-socket.addEventListener("message", (message) => {
-    const li = document.createElement("li");
-    li.innerText = message.data;
-
-    messageList.append(li);
-});
-
-//서버와 연결이 끊길때 이벤트
-socket.addEventListener("close", () => {
-    console.log("Disconnected from Server 😡");
-});
-
-
-function handleSubmit(event){
-    event.preventDefault();     //기본 동작을 안하게 하나봄. 체크박스 체크하면 bool값이 reverse되는것을 막는다거나
-    const input = messageForm.querySelector("input");
-
-    // console.log(input.value);
-    // socket.send(input.value);
-    socket.send(makeMessage("new_message", input.value));
-
-    //내가 보낸 메세지 화면에 그리기
-    const li = document.createElement("li");
-    li.innerText = `YOU : ${input.value}`;
-    messageList.append(li);
-
-
-    input.value = "";
-}
-
-function handleNickSubmit(event){
-    event.preventDefault();
-    const input = nickForm.querySelector("input");
-    // socket.send(input.value);
-    // socket.send(JSON.stringify({
-    //     type:"nickname",
-    //     payload:input.value,
-    // }));
-    socket.send(makeMessage("nickname", input.value));
-    
-}
-
-messageForm.addEventListener("submit", handleSubmit);
-nickForm.addEventListener("submit", handleNickSubmit);
-*/
-
-// socket.io
 const socket = io();
 
-const welcome = document.getElementById("welcome");
-const form = welcome.querySelector("form");
+const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
+const camerasSelect = document.getElementById("cameras");
 
-const room = document.getElementById("room");
+let myStream;
+let muted = false;
+let cameraOff = false;
 
-room.hidden = true;
-
-let roomName;
-
-function addMessage(message){
-    const ul = room.querySelector("ul");
-    const li = document.createElement("li");
-    li.innerHTML = message;
-    ul.appendChild(li);
+async function getCameras(){
+    try {
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        // console.log(devices);        //겁나 많이 나옴
+        const cameras = devices.filter(device => device.kind === "videoinput");
+        // console.log(cameras);
+        cameras.forEach(camera => {
+            const option = document.createElement("option");
+            option.value = camera.deviceId;
+            option.innerText = camera.label;
+            camerasSelect.appendChild(option);
+        })
+    } catch (e) {
+        console.log(e);
+    }
 }
 
-function handleMessageSubmit(event){
-    event.preventDefault();
-    const input = room.querySelector("#msg input");
-    const value = input.value;
-    socket.emit("new_message", input.value, roomName, () => {
-        addMessage(`You: ${value}`);
-    });        //이벤트 이름은 상관 없음.
-    input.value = "";
+async function getMedia(){
+    try {
+        myStream = await navigator.mediaDevices.getUserMedia({
+                audio: true,
+                video: true,
+        }); //인자로는 constraints를 보냄. constraints는 기본적으로 우리가 얻고싶은것
+        // console.log(myStream);
+        myFace.srcObject = myStream;
+
+        await getCameras();
+    } catch(e){
+        console.log(e);
+    }
 }
 
-function handleNicknameSubmit(event){
-    event.preventDefault();
-    const input = document.querySelector("#name input");    //room에서 id가 name인 오브젝트의 input
-    socket.emit("nickname", input.value);
+getMedia();
+
+//음소거 여부를 추적할 수 있는 variable이 필요.
+function handleMuteClick (){
+    // console.log(myStream.getAudioTracks());
+    myStream
+        .getAudioTracks()
+        .forEach((track) => (track.enabled = !track.enabled));
+
+    if(!muted){
+        muteBtn.innerText = "Unmute"
+    } else {
+        muteBtn.innerText = "Mute"
+    } 
+    muted = !muted;    
 }
+function handleCameraClick (){
+    console.log(myStream.getVideoTracks());
+    myStream
+        .getVideoTracks()
+        .forEach((track) => (track.enabled = !track.enabled));
 
-function showRoom(){    
-    welcome.hidden = true;
-    room.hidden = false;
-
-    const h3 = document.querySelector("h3");
-    h3.innerText = `Room ${roomName}`;
-    
-    const msgForm = room.querySelector("#msg");
-    msgForm.addEventListener("submit", handleMessageSubmit);    
+    if(cameraOff){
+        cameraBtn.innerText = "Turn Camera Off";
+    } else {
+        cameraBtn.innerText = "Turn Camera On";
+    } cameraOff = !cameraOff;    
 }
-
-function handleRoomSubmit (event) {
-    event.preventDefault();
-    const input = form.querySelector("input");
-
-    //이젠 socket.send가 아님
-    socket.emit(
-        "enter_room", 
-        // { payload: input.value }, 
-        input.value,    //꼭 2번째인자가 사전자료형일 필요 없음.
-        showRoom,
-    );   
-    //항상 메세지를 보낼 필요없음. emit으로 오브젝트 보낼 수 있음.   
-    //특정한 이벤트를 emit 할 수 있다. 어떤 이름이든 상관없음.
-    //object를 보낼 수 있다. string 아니여도 됨.
-
-    roomName = input.value;
-
-    input.value = "";
-}
-
-form.addEventListener("submit", handleRoomSubmit);
-
-const nameForm = document.querySelector("#name");
-nameForm.addEventListener("submit", handleNicknameSubmit);
-
-
-
-socket.on("welcome", (user, newCount) => {
-    const h3 = room.querySelector("h3");
-    h3.innerText = `Room ${roomName} (${newCount})`;
-    addMessage(`${user} joined!`);
-});
-
-socket.on("bye", (left, newCount) => {
-    const h3 = room.querySelector("h3");
-    h3.innerText = `Room ${roomName} (${newCount})`;
-    addMessage(`${left} left ㅠㅠ`);
-});
-
-
-// socket.on("new_message", (msg) => addMessage(msg + '-익명함수'));
-socket.on("new_message", addMessage);
-
-socket.on("room_change", console.log);
-// socket.on("room_change", (msg) => console.log(msg));     //위와 동일한 코드
-
-socket.on("room_change", (rooms) => {
-    const roomList = welcome.querySelector("ul");
-    roomList.innerHTML = "";
-    rooms.forEach(room => {
-        const li = document.createElement("li");
-        li.innerHTML = room;
-        roomList.append(li);
-    });
-});
+muteBtn.addEventListener("click", handleMuteClick);
+cameraBtn.addEventListener("click", handleCameraClick);
--- src/server.js
+++ src/server.js
@@ -1,11 +1,7 @@
 import http from "http";
 import {Server} from "socket.io";
-import { instrument } from "@socket.io/admin-ui";
 import express from "express";
-// import express, { application } from "express";
-// import { Socket } from "dgram";
-// import WebSocketServer from "ws";
-// import WebSocket from "ws";
+
 
 const app = express();
 
@@ -15,130 +11,8 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.render("home"));
 
-
-//웹소켓
-// const server = http.createServer(app);
-// const wss = new WebSocketServer.Server({ server });
-// const wss = new WebSocket.Server({ server });
-
-
-//socketIO
-// const server = http.createServer(app);
-const httpServer = http.createServer(app);      //이름은 자유
-const wsServer = new Server(httpServer, {
-    //데모가 작동하는데 필요한 환경설정
-    cors: {
-        origin: ["https://admin.socket.io"],
-        credentials: true,
-    }
-});        
-// const wsServer = SocketIO(httpServer);        
-instrument(wsServer, {
-    auth: false,
-    mode: "development",
-  });
-
-
-//publicRooms만 골라낸다. (socket id 걸러내기)
-function publicRooms(){
-    const {
-        sockets: {
-            adapter: {sids, rooms },
-        },
-    } = wsServer;      //이 문법은 많이 낯설다.
-    // const sids = wsServer.sockets.adapter.sids;
-    // const rooms = wsServer.sockets.adapter.rooms;
-
-    const publicRooms = [];
-    rooms.forEach((_, key) => {
-        if(sids.get(key) === undefined){
-            publicRooms.push(key);
-        }
-    })
-    return publicRooms;
-}
-
-function countRoom(roomName){
-    return wsServer.sockets.adapter.rooms.get(roomName)?.size;
-}
-
-//connection 받을 준비 끝
-wsServer.on("connection", (socket) => {
-    console.log(wsServer.sockets.adapter);
-    socket["nickname"] = "Anon";
-
-    socket.onAny((event) => {
-        console.log(`Socket Event:${event}`);
-        // console.log(`You are in room : ${JSON.stringify(socket.rooms)}`);    //이건 아닌가봄.
-    })
-
-    socket.on("enter_room", (roomName, done) => {
-        socket.join(roomName);
-        done();
-        socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
-        wsServer.sockets.emit("room_change", publicRooms());    //서버의 모든 소켓에 public 방들에 대한 정보를 payload로 보낸다.
-    });
-
-    socket.on("disconnecting", () => {
-        socket.rooms.forEach(
-            room => socket.to(room).emit("bye", socket.nickname, countRoom(room) -1)
-        );
-        wsServer.sockets.emit("room_change", publicRooms()); 
-        //브라우저를 끄지 않고 새로고침만 하면, 바로 소켓이 사라지지 않는걸까
-        //가 아니라 한발 늦게 반응함. disconnected된게 아니라, disconnecting. 완전 사라지지 않았기 때문인듯.
-    });
-
-    socket.on("disconnect", () => {
-        wsServer.sockets.emit("room_change", publicRooms()); 
-    });
-
-    socket.on("new_message", (msg, roomName, done) => {
-        socket.to(roomName).emit("new_message", `${socket.nickname}: ${msg}`);
-        done();
-    });
-
-    socket.on("nickname", nickname => socket["nickname"] = nickname);
-});
-
-
-
-//기존 웹소켓 활용 코드
-/*
-const wss = new WebSocket.Server({ server });
-const sockets = [];
-
-//web socket도 event가 있다.
-wss.on("connection", (socket) => {
-    sockets.push(socket);
-    socket["nickname"] = "Anon";
-
-    //클라이언트랑 연결이 끊기면 동작
-    socket.on("close", () => {console.log("Disconnected from the Browser 😡");});
-    // socket.on("close", onSocketClose);
-
-    //클라이언트에게서 메세지 받음
-    socket.on("message", (msg) => {
-        const message = JSON.parse(msg);
-
-        switch (message.type){
-            case "new_message":
-                //그냥 바꿔봤는데 되네...?
-                sockets.forEach(aSocket => {aSocket === socket ? {} : aSocket.send(`${socket.nickname} : ${message.payload}`)});
-                break;
-            case "nickname":
-                // console.log(message.payload);
-                socket["nickname"] = message.payload;
-                break;
-        }
-    });
-
-    //클라이언트에게 메세지 전송
-    // socket.send("hello!!!");
-
-    console.log("Connected to Browser ✔");
-});
-*/
+const httpServer = http.createServer(app);  
+const wsServer = new Server(httpServer);        
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-httpServer.listen(3000, handleListen);      //포트번호, 함수 실행
-
+httpServer.listen(3000, handleListen);
